3e0400354ec144e1722062f8f614d60f15b58c7c
yonatan <yonivak1@gmail.com>
2019-12-27 13:21:05 +0200

    Added custom user profile model, manager after fixed

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -26,7 +26,7 @@ class UserProfileManager(BaseUserManager):
 
         user.is_superuser = True
         user.is_staff = True
-        user.seve(using=self._db)
+        user.save(using=self._db)
 
         return user
 
